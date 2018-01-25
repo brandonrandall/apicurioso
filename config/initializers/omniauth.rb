0661714e6f5f161172bcdc53a03dc9d480f8e049
Brandon Randall <br.mhcexchange@gmail.com>
2018-01-24 21:14:47 -0700

    corrects github oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"]
+  provider :github, ENV["GITHUB_APP_ID"], ENV["GITHUB_SECRET"]
   provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
 end
